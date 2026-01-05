6e630c3967c26b10b3969bd7dde24762508ad7fc
Ann Riabykina <anny.r20.1994@gmail.com>
2026-01-05 19:42:51 +0200

    Fix views.py

--- cinema/views.py
+++ cinema/views.py
@@ -33,28 +33,24 @@ class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
-    http_method_names = ["get", "post", "head", "options"]
 
 
 class ActorViewSet(viewsets.ModelViewSet):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
     permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
-    http_method_names = ["get", "post", "head", "options"]
 
 
 class CinemaHallViewSet(viewsets.ModelViewSet):
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
     permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
-    http_method_names = ["get", "post", "head", "options"]
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.prefetch_related("genres", "actors")
     serializer_class = MovieSerializer
     permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
-    http_method_names = ["get", "post", "head", "options"]
 
     @staticmethod
     def _params_to_ints(qs):
@@ -104,7 +100,6 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
     )
     serializer_class = MovieSessionSerializer
     permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
-    http_method_names = ["get", "post", "head", "options"]
 
     def get_queryset(self):
         date = self.request.query_params.get("date")
@@ -143,7 +138,6 @@ class OrderViewSet(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
     pagination_class = OrderPagination
     permission_classes = [permissions.IsAuthenticated]
-    http_method_names = ["get", "post", "head", "options"]
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
--- user/serializers.py
+++ user/serializers.py
@@ -23,9 +23,28 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=False,
+        min_length=5
+    )
+
     class Meta:
         model = User
-        fields = ("id", "username", "email", "is_staff")
+        fields = ("id", "username", "email", "password", "is_staff")
+        read_only_fields = ("is_staff",)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if password:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
 
 
 class UserLoginSerializer(serializers.Serializer):
