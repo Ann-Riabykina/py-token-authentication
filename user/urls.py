9881ea06208e581d00bf08460b6511238e3977d2
Ann Riabykina <anny.r20.1994@gmail.com>
2026-01-05 19:12:16 +0200

    Fix urls.py

--- user/urls.py
+++ user/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import UserRegisterView, UserLoginView, UserMeView
 
 urlpatterns = [
-    path("register/", UserRegisterView.as_view(), name="user-register"),
-    path("login/", UserLoginView.as_view(), name="user-login"),
-    path("me/", UserMeView.as_view(), name="user-me"),
+    path("register/", UserRegisterView.as_view(), name="create"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("me/", UserMeView.as_view(), name="me"),
 ]
 
 app_name = "user"
